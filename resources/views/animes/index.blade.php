2039057bc0a4a89dd66f29041e13a84c1e8a3f84
Icaro Apolo <apoloraci@gmail.com>
2022-04-20 21:51:16 -0300

    add input in anime anime for update title

--- resources/views/animes/index.blade.php
+++ resources/views/animes/index.blade.php
@@ -17,20 +17,46 @@
     <ul class="list-group">
         @foreach ($animes as $anime)
             <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span id="name-anime-{{ $anime->id }}">{{ $anime->name }}</span>
+
+                <div class="input-group w-50" hidden id="input-name-anime-{{ $anime->id }}">
+                    <input type="text" class="form-control" value="{{ $anime->name }}">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" onclick="editAnime({{ $anime->id }})">
+                            <i class="fas fa-check"></i>
+                        </button>
+                        @csrf
+                    </div>
+                </div>
                 
-                {{ $anime->name }}
                 <span class="d-flex">
-                    <a href="/animes/{{ $anime->id }}/temporadas" class="btn btn-info btn-sm m-2">
+                    <button class="btn btn-info btn-sm m-1" onclick="toggleInput({{ $anime->id }})">
+                        <i class="fas fa-edit"></i>
+                    </button>
+                    <a href="/animes/{{ $anime->id }}/temporadas" class="btn btn-info btn-sm m-1">
                         <i class="fas fa-external-link-alt"></i>
                     </a>
                     <form method="post" action="/animes/{{$anime->id}}"
                         onsubmit="return confirm('Are you sure you want to delete?')" >
                         @csrf
                         @method('DELETE')
-                        <button class="btn btn-danger btn-sm m-2"><i class="fa-solid fa-trash-can"></i></button>
+                        <button class="btn btn-danger btn-sm m-1"><i class="fa-solid fa-trash-can"></i></button>
                     </form>
                 </span>
             </li>
         @endforeach
     </ul>
+<script>
+    function toggleInput(animeId) {
+        const nameAnimeEl = document.getElementById(`name-anime-${animeId}`);
+        const inputAnimeEl = document.getElementById(`input-name-anime-${animeId}`);
+        if (nameAnimeEl.hasAttribute('hidden')) {
+            nameAnimeEl.removeAttribute('hidden');
+            inputAnimeEl.hidden = true;
+        } else {
+            inputAnimeEl.removeAttribute('hidden');
+            nameAnimeEl.hidden = true;
+        }
+    }
+</script>
 @endsection
