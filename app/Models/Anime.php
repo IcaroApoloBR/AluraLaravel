97648f39dc71a47dcadf278de10bf133860bcc92
Icaro Apolo <apoloraci@gmail.com>
2022-04-30 13:33:49 -0300

    show anime picture into page season

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage as FacadesStorage;
+use Storage;
 
 class Anime extends Model
 {
     public $timestamps = false;
     protected $fillable = ['name','picture'];
+
+    public function getPictureUrlAttribute(){
+        if($this->picture)
+        {
+            return FacadesStorage::url($this->picture);
+        }
+        return FacadesStorage::url('anime/sem-imagem.jpg');
+    }
 
     public function seasons() {
         return $this->hasMany(Season::class);
